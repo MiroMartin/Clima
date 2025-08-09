5f83278d291eb2dc9e7e0052877104e8beffa713
MartinMiro <mmiro578@gmail.com>
2025-08-09 00:15:21 -0300

    Primet commit

--- main.go
+++ main.go
@@ -13,5 +13,5 @@ func main() {
 	var datos jsonsave.Datos = jsonsave.JsonSaver(json)
 	termperatura := datos.Current.Temperature_2m
 	sensacion_termica := datos.Current.Apparent_temperature
-	fmt.Printf("La temperatura actual es %.1f y la sensacion termica es %.1f\n", termperatura, sensacion_termica)
+	fmt.Printf("La temperatura actual en San Clemente es %.1f y la sensacion termica es %.1f\n", termperatura, sensacion_termica)
 }
